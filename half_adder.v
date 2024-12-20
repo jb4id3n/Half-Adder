9aa8cb32580c74628e776f9075314d85cb5d2cdf
Josephine Baiden <115303043+jb4id3n@users.noreply.github.com>
2024-12-19 18:00:57 -0800

    Update half_adder.v

--- half_adder.v
+++ half_adder.v
@@ -1,24 +1,4 @@
 `timescale 1ns / 1ps
-//////////////////////////////////////////////////////////////////////////////////
-// Company: 
-// Engineer: 
-// 
-// Create Date: 12/15/2024 04:01:49 PM
-// Design Name: 
-// Module Name: half_adder
-// Project Name: 
-// Target Devices: 
-// Tool Versions: 
-// Description: 
-// 
-// Dependencies: 
-// 
-// Revision:
-// Revision 0.01 - File Created
-// Additional Comments:
-// 
-//////////////////////////////////////////////////////////////////////////////////
-
 // Design Source File
 module half_adder(a, b, sum, carry,); // Declaring the variables used in project
 
